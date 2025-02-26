1ec1edb128307b6f4d8c8a1a8f0afca59ce7ecb4
Syrinth <evan.ouell@gmail.com>
2025-02-25 19:06:40 -0500

    Routing

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const pinoLogger = require('./logger');
+const giftRoutes = require('./routes/giftRoutes');
 
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
@@ -23,7 +24,7 @@ app.use(express.json());
 
 // Route files
 // Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
-//{{insert code here}}
+app.use('/api/gifts', giftRoutes);
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 //{{insert code here}}
