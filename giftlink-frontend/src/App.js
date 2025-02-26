17ef7715ae35e090aaa0cf477576723a261f2bd3
Syrinth <evan.ouell@gmail.com>
2025-02-26 13:19:10 -0500

    Register and Login

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
+import LoginPage from './components/LoginPage/LoginPage';
+import RegisterPage from './components/RegisterPage/RegisterPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
@@ -11,9 +13,11 @@ function App() {
     <>
         <Navbar/>
         <Routes>
-          {/* the final code will not pass the products to every page, but each page will call the server API */}
-          <Route path="/" element={<MainPage />} />
-          <Route path="/app" element={<MainPage />} />
+            {/* the final code will not pass the products to every page, but each page will call the server API */}
+            <Route path="/" element={<MainPage />} />
+            <Route path="/app" element={<MainPage />} />
+            <Route path="/app/login" element={<LoginPage/>} />
+            <Route path="/app/register" element={<RegisterPage />} />
         </Routes>
         </>
   );
