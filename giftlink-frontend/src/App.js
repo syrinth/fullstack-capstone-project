900c6568ed58e29e70602d51010029e30df1957d
Syrinth <evan.ouell@gmail.com>
2025-02-26 13:31:08 -0500

    Details

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -18,6 +18,7 @@ function App() {
             <Route path="/app" element={<MainPage />} />
             <Route path="/app/login" element={<LoginPage/>} />
             <Route path="/app/register" element={<RegisterPage />} />
+            <Route path="/app/product/:productId" element={<DetailsPage/>} />
         </Routes>
         </>
   );
